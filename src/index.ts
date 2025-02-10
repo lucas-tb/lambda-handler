0d911c4bb2f00f39cef0bd64ed7aa25ebd1d7f6d
lucas-tb <lucas.euzebio@taniabulhoes.com.br>
2025-02-10 17:44:43 -0300

    Install AWS Lambda types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export const handler = async (_event: any) => {{
+import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda'
+
+export const handler = async (_event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {{
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!')
